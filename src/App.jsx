a70ca2b72344c81677b7a3b64c7cdad341da5611
arany677 <arany.cse.20220204053@aust.edu>
2025-02-11 00:28:15 +0600

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home.jsx";
 
 function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    <div className="mx-4 sm:mx-[10%] ">
+      <Routes>
+        <Route path="/" elemnts={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,72 +6,40 @@ function Navbar() {
       <div>
         <div className="navbar bg-base-100">
           <div className="navbar-start">
-            <div className="dropdown">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost lg:hidden"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />
-                </svg>
-              </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-              >
-                <li>
-                  <a>Item 1</a>
-                </li>
-                <li>
-                  <a>Parent</a>
-                  <ul className="p-2">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a>Item 3</a>
-                </li>
-              </ul>
-            </div>
             <a className="btn btn-ghost text-xl">daisyUI</a>
           </div>
-          <div className="navbar-center hidden lg:flex">
+          <div className="navbar-center">
+            {" "}
+            {/* Removed 'hidden lg:flex' to always show menu */}
             <ul className="menu menu-horizontal px-1">
               <li>
-                <a>Item 1</a>
+                <a className="border-b-2 border-transparent hover:border-blue-500 pb-1 transition-colors duration-200">
+                  Item 1
+                </a>
               </li>
               <li>
                 <details>
-                  <summary>Parent</summary>
+                  <summary className="border-b-2 border-transparent hover:border-blue-500 pb-1 transition-colors duration-200">
+                    Parent
+                  </summary>
                   <ul className="p-2">
                     <li>
-                      <a>Submenu 1</a>
+                      <a className="border-b-2 border-transparent hover:border-blue-500 pb-1 transition-colors duration-200">
+                        Submenu 1
+                      </a>
                     </li>
                     <li>
-                      <a>Submenu 2</a>
+                      <a className="border-b-2 border-transparent hover:border-blue-500 pb-1 transition-colors duration-200">
+                        Submenu 2
+                      </a>
                     </li>
                   </ul>
                 </details>
               </li>
               <li>
-                <a>Item 3</a>
+                <a className="border-b-2 border-transparent hover:border-blue-500 pb-1 transition-colors duration-200">
+                  Item 3
+                </a>
               </li>
             </ul>
           </div>
